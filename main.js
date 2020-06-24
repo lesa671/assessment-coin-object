6332607c8e23b044afe08f86bd00f79377a7434d
Lesa Ducksworth <lducks67@gmail.com>
2020-06-24 02:14:24 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,48 @@
+//transparency - had some assistance from classmate Colin
+
+let coin = {
+    state: 0,
+    flip: function () {
+        return this.state = Math.floor(Math.random() * 2)
+    },
+    toString: function () {
+        if (this.state === 0) {
+            return 'Heads'
+        } else {
+            return 'Tails'
+        }
+    },
+    toHTML: function () {
+        let image = document.createElement('img')
+        if (this.state === 0) {
+            image.src = './images/Kennedy half dollar heads.jpg'
+        } else {
+            image.src = './images/2020-P-Kennedy-Half-Dollar-reverse.jpg'
+        }
+        return image
+    }
+};
+
+const bodyElement = document.querySelector('body')
+const mainElement = document.createElement('main')
+bodyElement.append(mainElement)
+
+function display20Flips() {
+    for (let index = 0; index < 20; index++) {
+        let showFlip = document.createElement('p')
+        coin.flip()
+        showFlip.append(coin.toString())
+        mainElement.append(showFlip)
+    }
+}
+console.log(display20Flips())
+
+function display20Images() {
+    for (let index = 0; index < 20; index++) {
+        coin.flip()
+        coin.toHTML()
+        mainElement.append(coin.toHTML())
+    }
+}
+console.log(display20Images())
+
